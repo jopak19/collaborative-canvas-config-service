283503a61db87aab02408401a10b3d04e486d764
jopak19 <jpaulogomessiqueira@gmail.com>
2023-06-22 10:20:55 -0300

    criação de server cloud config

---
+++ config-service/src/test/java/br/ufrn/dimap/collaborativecanvas/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.dimap.collaborativecanvas.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
